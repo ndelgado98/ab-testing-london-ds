3d72129d8005a2147e09f3fbe374b541ac60dce6
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2020-03-12 10:59:00 +0000

    08

--- create_dummy_data.R
+++ create_dummy_data.R
@@ -8,3 +8,16 @@ df <- data.frame(baseline, experiment)
 write.csv(df, file = "experiment_data.csv")
 
 t.test(df$baseline, df$experiment, var.equal = FALSE)
+
+
+#--------------------------------------------------------
+
+set.seed(100)
+
+baseline_small <- rnorm(n = 30, mean = 4.15,sd = 12)
+experiment_small <- rnorm(n = 30, mean = 10.82,sd = 17)
+
+df_small <- data.frame(baseline_small, experiment_small)
+t.test(df_small$baseline, df_small$experiment, var.equal = FALSE)
+
+write.csv(df_small, file = "experiment_data_small.csv")
